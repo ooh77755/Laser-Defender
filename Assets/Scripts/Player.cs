aba3ab3580f2c4d23be5b413caad17fc111a9be3
Alan Stewart <alan.stewart@chi.ac.uk>
2021-07-28 13:29:42 +0100

    Added 'player.cs' / added code to let player move along x axis

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float speed = 10f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        var newXPos = transform.position.x + deltaX;
+        transform.position = new Vector2(newXPos, transform.position.y);
+    }
+
+}
