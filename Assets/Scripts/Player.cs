bbbd78298a133021030305b74c7a6be7bb36cd15
Alan Stewart <alan.stewart@chi.ac.uk>
2021-07-28 16:03:46 +0100

    Set up screen boundaries on x & y axes (+padding) using ViewPortToWorldPoint()

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     float speed = 10f;
-    
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+    float padding = 0.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpMoveBoundaries();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 
     // Update is called once per frame
@@ -21,10 +35,9 @@ public class Player : MonoBehaviour
     void Move()
     {
         var deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        var newXPos = transform.position.x + deltaX;
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
         var deltaY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        var newYPos = transform.position.y + deltaY;
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
         transform.position = new Vector2(newXPos, newYPos);
     }
-
 }
